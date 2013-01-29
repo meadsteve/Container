a851d5c0b7c32487e2e116645faa9b35e44ec66f
meadsteve <meadsteve@gmail.com>
2013-01-29 20:40:07 +0000

    First commit of a very basic injection container that I've used for a few small projects.

---
+++ src/MeadSteve/Container/Container.php
@@ -0,0 +1,199 @@
+<?php
+namespace Meadsteve\Container;
+
+/**
+ * @package    Depedency Injection Container
+ * @author     Steve B <meadsteve@gmail.com>
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+/**
+ * Basic dependency injection container.
+ */
+class Container {
+
+	/**
+	 * All of the resource loaders registered in the container
+	 * @var callable[]
+	 */
+	protected $arrRegisteredCallbacks = array();
+
+	/**
+	 * All the single items registered with the container
+	 * @var array
+	 */
+	protected $arrRegisteredItems = array();
+
+	/**
+	 * Controls whether new resources and loaders can be set.
+	 * @var bool
+	 */
+	protected $LockedFromChanges = false;
+
+	/**
+	 * Registers a resource with the container. If the resource is callable
+	 * then it will be invoked every time with this container as the only argument.
+	 * If the callable itself should be returned then set $ForceDisableLoader to true
+	 * @param string $Name
+	 * @param mixed $Thing
+	 * @param bool $ForceDisableLoader Stops a callable $Thing being registered as a loader
+	 * @throws \InvalidArgumentException|\RuntimeException
+	 */
+	public function Register($Name, $Thing, $ForceDisableLoader = false) {
+
+		if ($this->LockedFromChanges) {
+			throw new \RuntimeException(
+				"Can't register resources when container is locked"
+			);
+		}
+
+		if (!is_string($Name)) {
+			throw new \InvalidArgumentException('Name must be a string');
+		}
+
+		$this->UnsetResource($Name);
+
+		if (is_callable($Thing) && !$ForceDisableLoader) {
+			$this->RegisterCallback($Name, $Thing);
+		}
+		else {
+			$this->RegisterItem($Name, $Thing);
+		}
+	}
+
+
+	/**
+	 * Returns the resource in this container for $Name
+	 * @param string $Name
+	 * @return mixed
+	 * @throws \OutOfRangeException if $Name doesn't exist
+	 */
+	public function GetResource($Name) {
+		if ($this->CallbackExists($Name)) {
+			$Callback = $this->GetCallback($Name);
+			return $Callback($this);
+		}
+		else if ($this->ItemExists($Name)) {
+			return $this->GetItem($Name);
+		}
+
+		throw new \OutOfRangeException('Resource not found for ' . $Name);
+	}
+
+	/**
+	 * Returns the actual instance of the callback used to load the resource $Name
+	 * @param string $Name
+	 * @throws \OutOfRangeException if the specified name doesn't exist
+	 * @return callable
+	 */
+	public function &GetResourceLoader($Name) {
+		if ($this->CallbackExists($Name)) {
+			return $this->GetCallback($Name);
+		}
+		else {
+			throw new \OutOfRangeException($Name . "doesn't exist as a loader");
+		}
+	}
+
+	/**
+	 * Locks the container so that new resources can't be registered.
+	 * @param bool $Lock
+	 */
+	public function Lock($Lock = true) {
+		$this->LockedFromChanges = ($Lock !== false);
+	}
+
+	/**
+	 * Loads an item from the container
+	 * @param $Name
+	 * @throws \OutOfRangeException if the specified name doesn't exist
+	 * @return mixed
+	 */
+	public function __get($Name) {
+		return $this->GetResource($Name);
+	}
+
+	/**
+	 * Registers an item with the container.
+	 * @param $Name
+	 * @param $Value
+	 */
+	public function __set($Name, $Value) {
+		$this->Register($Name, $Value);
+	}
+
+	/**
+	 * @param $Name
+	 */
+	protected function UnsetResource($Name) {
+		if ($this->CallbackExists($Name)) {
+			$this->UnsetCallback($Name);
+		}
+		else if ($this->ItemExists($Name)) {
+			$this->UnsetItem($Name);
+		}
+	}
+
+	/**
+	 * @param string $Name
+	 * @param callable $Callback
+	 */
+	protected function RegisterCallback($Name, callable $Callback) {
+		$this->arrRegisteredCallbacks[$Name] = $Callback;
+	}
+
+	/**
+	 * @param string $Name
+	 * @param mixed $Item
+	 */
+	protected function RegisterItem($Name, $Item) {
+		$this->arrRegisteredItems[$Name] = $Item;
+	}
+
+
+	/**
+	 * @param string $Name
+	 * @return bool
+	 */
+	protected function CallbackExists($Name) {
+		return array_key_exists($Name, $this->arrRegisteredCallbacks);
+	}
+
+	/**
+	 * @param string $Name
+	 * @return callable
+	 */
+	protected function GetCallback($Name) {
+		return $this->arrRegisteredCallbacks[$Name];
+	}
+
+	/**
+	 * @param string $Name
+	 */
+	protected function UnsetCallback($Name) {
+		unset($this->arrRegisteredCallbacks[$Name]);
+	}
+
+	/**
+	 * @param string $Name
+	 * @return bool
+	 */
+	protected function ItemExists($Name) {
+		return array_key_exists($Name, $this->arrRegisteredItems);
+	}
+
+	/**
+	 * @param string $Name
+	 * @return mixed
+	 */
+	protected function GetItem($Name) {
+		return $this->arrRegisteredItems[$Name];
+	}
+
+	/**
+	 * @param string $Name
+	 */
+	protected function UnsetItem($Name) {
+		unset($this->arrRegisteredItems[$Name]);
+	}
+}
---
+++ src/MeadSteve/Container/Singleton.php
@@ -0,0 +1,67 @@
+<?php
+namespace Meadsteve\Container;
+
+/**
+ * @package    Depedency Injection Container
+ * @author     Steve B <meadsteve@gmail.com>
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+/**
+ * The singleton class is a small companion class for the MeadSteve\Container
+ * Dependency injection container. It allows a resource to be wrapped up
+ * as a singleton so that any construction logic it may have is only run a single
+ * time.
+ */
+class Singleton {
+
+	/**
+	 * The function responsible for creating the resource instance for this singleton.
+	 * @var callable
+	 */
+	protected $Callback;
+
+	/**
+	 * The specific instance represented by the singleton once the loader has been
+	 * called.
+	 * @var mixed
+	 */
+	protected $LoadedInstance;
+
+	/**
+	 * @param callable $WrappedCallback Function that creates the singleton resource.
+	 */
+	public function __construct(callable $WrappedCallback) {
+		$this->Callback = $WrappedCallback;
+		$this->LoadedInstance = null;
+	}
+
+	/**
+	 * Returns the single instance of the resource represented by this singleton.
+	 * @param Container $Cont
+	 * @return mixed
+	 */
+	public function GetInstance(Container $Cont = null) {
+		if ($this->LoadedInstance === null) {
+			$Callback = $this->Callback;
+			$this->LoadedInstance = $Callback($Cont);
+		}
+		return $this->LoadedInstance;
+	}
+
+	/**
+	 * Forces the singleton instance to be reloaded next time it's requested.
+	 */
+	public function ForceReload() {
+		$this->LoadedInstance = null;
+	}
+
+	/**
+	 * When called as a function the singleton returns the instance it represents.
+	 * @param mixed $Arg
+	 * @return mixed
+	 */
+	public function __invoke($Arg = null) {
+		return $this->GetInstance($Arg);
+	}
+}
